59d7d1512992805515fec028ccc7fd5e372e9ca3
Moridin830 <94042061+Moridin830@users.noreply.github.com>
2022-01-06 10:00:44 +0300

    Update Program.cs
    Добавление комментариев.

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -11,20 +11,29 @@ namespace Task2
         static void Main()
         {
 
+            // Инициализируем логгер
             Logger = new Logger();
 
+            // Запускаем процесс ввода значений и расчета суммы
             PerformCalc(Logger);
             
         }
 
+        /// <summary>
+        /// Метод, запрашивающий у пользователя ввод двух значений и выводящий их сумму
+        /// </summary>
+        /// <param name="Logger"></param>
         private static void PerformCalc(ILogger Logger)
         {
-            
+
+            // Инициализируем объект калькулятора
             Calc myCalc = new();
 
+            // Запрашиваем у пользователя ввод параметров
             UserAnswers.ReadUserAnswer("Input 'X': ", out double x, Logger);
             UserAnswers.ReadUserAnswer("Input 'Y': ", out double y, Logger);
 
+            // Выводим результат
             Console.WriteLine($"Result: {myCalc.Sum(x, y)}");
 
             Console.WriteLine("Press any key...");
