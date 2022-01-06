40d6e31c9f5b59d29a7d4dc4883e5f39f59690e8
Moridin830 <94042061+Moridin830@users.noreply.github.com>
2022-01-06 10:01:16 +0300

    Update UserAnswers.cs
    Добавление комментариев.

--- Task2/UserAnswers.cs
+++ Task2/UserAnswers.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,22 +8,33 @@ namespace Task2
 {
     public class UserAnswers
     {
+        /// <summary>
+        /// Метод, запрашивающий у пользователя ввод значения и преобразующий введенное значение в тип 'double'
+        /// </summary>
+        /// <param name="Question"></param>
+        /// <param name="Answer"></param>
+        /// <param name="Logger"></param>
         public static void ReadUserAnswer(string Question, out double Answer, ILogger Logger)
         {
             Console.Write(Question);
+            
+            // Читаем введенное пользователем значение
             string UserAnswer = Console.ReadLine();
 
             try
             {
+                // Пытаемся преобразовать введенное пользователем значение в double
                 Answer = Convert.ToDouble(UserAnswer);
             }
             catch (Exception ex) when (ex is ArgumentNullException)
             {
+                // Выводим лог ошибки при вводе пустого значения
                 Logger.Error("Empty values cannot be entered");
                 ReadUserAnswer(Question, out Answer, Logger);
             }
             catch
             {
+                // Выводим лог ошибки при вводе некорректного значения
                 Logger.Error("Incorrect value");
                 ReadUserAnswer(Question, out Answer, Logger);
             }
