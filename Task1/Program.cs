a7288f4510c521543c6bb99db36561d33aeced08
Moridin830 <94042061+Moridin830@users.noreply.github.com>
2022-01-06 09:57:19 +0300

    Update Program.cs
    Добавление комментариев.

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -10,18 +10,25 @@ namespace Task1
         static void Main()
         {
 
+            // Запускаем процесс ввода значений и расчета суммы
             PerformCalc();
             
         }
 
+        /// <summary>
+        /// Метод, запрашивающий у пользователя ввод двух значений и выводящий их сумму
+        /// </summary>
         private static void PerformCalc()
         {
             
+            // Инициализируем объект калькулятора
             Calc myCalc = new();
 
+            // Запрашиваем у пользователя ввод параметров
             UserAnswers.ReadUserAnswer("Input 'X': ", out double x);
             UserAnswers.ReadUserAnswer("Input 'Y': ", out double y);
 
+            // Выводим результат
             Console.WriteLine($"Result: {myCalc.Sum(x, y)}");
 
             Console.WriteLine("Press any key...");
