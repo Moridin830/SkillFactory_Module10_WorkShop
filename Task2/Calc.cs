878745eb14ab48cddd070d7bf376586ea8c27037
Moridin830 <94042061+Moridin830@users.noreply.github.com>
2022-01-06 09:58:07 +0300

    Update Calc.cs
    Добавление комментариев.

--- Task2/Calc.cs
+++ Task2/Calc.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,14 +6,29 @@ using System.Threading.Tasks;
 
 namespace Task2
 {
+    /// <summary>
+    /// Класс для описания калькулятора
+    /// </summary>
     public class Calc : ICalc
     {
-        
+
+        /// <summary>
+        /// Метод, возвращающий сумму двух поступающих на вход параметров типа 'int'
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
         public int Sum(int x, int y)
         {
             return x + y;
         }
 
+        /// <summary>
+        /// Метод, возвращающий сумму двух поступающих на вход параметров типа 'double'
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
         public double Sum(double x, double y)
         {
             return x + y;
